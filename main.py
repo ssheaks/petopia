faf199c8afc6e014b21dbf56478b78218135c757
ssheaks <sarah.sheaks@gmail.com>
2018-06-05 20:53:31 -0700

    implement main.py game, add background image, create get image function and can add image to game

---
+++ main.py
@@ -0,0 +1,38 @@
+import sys
+import os
+import pygame as pg
+from pygame.locals import *
+
+_image_library = {}
+def get_image(path):
+        global _image_library
+        image = _image_library.get(path)
+        if image == None:
+                canonicalized_path = path.replace('/', os.sep).replace('\\', os.sep)
+                image = pg.image.load(canonicalized_path)
+                _image_library[path] = image
+        return image
+
+# image = py.image.load('images/Cartoon_Border_Collie.png')
+
+def run_game():
+  #Initialize and set up screen.
+  pg.init()
+  screen = pg.display.set_mode((1280, 948))
+  pg.display.set_caption("Petopia: Pet Rescue!")
+  #load background image
+  background_image = pg.image.load("picnic-area-149153_1280.png").convert()
+  #Start main loop.
+  while True: 
+    #start event loop.
+    for event in pg.event.get():
+      if event.type == pg.QUIT:
+        sys.exit()
+    #set background image
+    screen.blit(background_image, [0, 0])
+    #get dog image 
+    screen.blit(get_image('images/Cartoon_Border_Collie.png'), (20, 30))
+    #Refresh screen.
+    pg.display.flip()
+
+run_game()
